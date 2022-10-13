666427052a2d028a2440419b19a44dbabc8f7e75
Amit0129 <amitkumarnayak40@gmail.com>
2022-10-13 17:46:23 +0530

    Added: UC1 Check Employee is Present or Absent

--- Employee/Employee/Program.cs
+++ Employee/Employee/Program.cs
@@ -5,6 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Employ Wage Calculation");
+            int fullTime = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == fullTime)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee id absent");
+            }
         }
     }
 }
