92fef9a62a055509b33c980695b2d5a35a648ebf
Amit0129 <amitkumarnayak40@gmail.com>
2022-10-13 18:35:08 +0530

    Added: UC5 Calculating Wages for a Month

--- Employee/Employee/Program.cs
+++ Employee/Employee/Program.cs
@@ -7,32 +7,34 @@ namespace Employee
         public const int fullTime = 1;
         public const int partTime = 2;
         public const int empRatePerHour = 20;
+        public const int numOfWorkDay = 20;
         static void Main(string[] args)
         {
             Console.WriteLine("Employ Wage Calculation");
 
-            int empHrs = 0;
-            int empWage = 0;
+            int empHrs = 0, empWage = 0, totalEmpWage = 0;
 
-
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-
-
-            switch (empCheck)
+            for(int day = 0; day < numOfWorkDay; day++)
             {
-                case fullTime:
-                    empHrs = 8;
-                    break;
-                case partTime:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+
+                switch (empCheck)
+                {
+                    case fullTime:
+                        empHrs = 8;
+                        break;
+                    case partTime:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * empRatePerHour;
+                totalEmpWage += empWage;
             }
-            empWage = empHrs * empRatePerHour;
-            Console.WriteLine("Employ Wage is : " + empWage);
+            Console.WriteLine("Total Employ Wage is : " + totalEmpWage);
         }
     }
 }
