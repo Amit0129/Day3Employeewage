ac9adf39b8b2c6c4ed21db4a3ebfabe4049ac464
Amit0129 <amitkumarnayak40@gmail.com>
2022-10-17 14:52:50 +0530

    UC10 Added Support Employee Wage For Multiple Company Using Interface Approch

---
+++ Employee/Employee/CompanyEmpWage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    public class CompanyEmpWage
+    {
+        public string company;
+        public int empRatePerHour;
+        public int numOfWorkDay;
+        public int maxHourInMonth;
+        public int totalEmpWage;
+
+        public CompanyEmpWage(string company, int empRatePerHour, int numOfWorkDay, int maxHourInMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkDay = numOfWorkDay;
+            this.maxHourInMonth = maxHourInMonth;
+        }
+        public void setTotalEmpWage(int totalEmpWage)
+        {
+            this.totalEmpWage = totalEmpWage;
+        }
+        public string toString()
+        {
+            return "Totall Emp Wage For Company : " + this.company + " is " + this.totalEmpWage;
+        }
+    }
+
+}
---
+++ Employee/Employee/EmpWageBuilderArray.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    public class EmpWageBuilderArray
+    {
+        public const int partTime = 1;
+        public const int fullTime = 2;
+
+        private int numOfCompany = 0;
+        private CompanyEmpWage[] companyEmpWagesArray;
+
+        public EmpWageBuilderArray()
+        {
+            this.companyEmpWagesArray = new CompanyEmpWage[5];
+        }
+        public void addCompanyWage(string company, int empRatePerHour, int numOfWorkDay, int maxHourInMonth)
+        {
+            companyEmpWagesArray[this.numOfCompany] = new CompanyEmpWage(company, empRatePerHour, numOfWorkDay, maxHourInMonth);
+            numOfCompany++;
+        }
+        public void computeEmpWage()
+        {
+            for (int i=0; i < numOfCompany; i++)
+            {
+                companyEmpWagesArray[i].setTotalEmpWage(this.computeEmpWage(this.companyEmpWagesArray[i]));
+                Console.WriteLine(this.companyEmpWagesArray[i].ToString());
+            }
+        }
+        private int computeEmpWage(CompanyEmpWage companyEmpWage)
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkDay = 0;
+
+            while (totalEmpHrs <= companyEmpWage.maxHourInMonth && totalWorkDay < companyEmpWage.numOfWorkDay)
+            {
+                totalWorkDay++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+
+                switch (empCheck)
+                {
+                    case fullTime:
+                        empHrs = 8;
+                        break;
+                    case partTime:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day" + totalWorkDay + "Employee Hour" + empHrs);
+            }
+            return totalEmpHrs * companyEmpWage.empRatePerHour;
+        }
+
+    }
+}
